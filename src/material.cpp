8402d0fadd001be02f20eb3ca01b1e5a6f0105c5
EscaperX <ycuangwhen@gmail.com>
2023-04-10 14:36:56 +0800

    Add some documents

--- src/material.cpp
+++ src/material.cpp
@@ -1,24 +1,36 @@
 #include "material.h"
 #include "intersection.h"
 #include "vector.h"
-inline float Luminance(Vector3f color) {
+inline float Luminance(Vector3f color)
+{
     return 0.212671f * color.x + 0.715160f * color.y + 0.072169f * color.z;
 }
-inline Vector3f spherical_dir(float sin_theta, float cos_theta, float phi) {
-    return Vector3f{ sin_theta * std::cos(phi), sin_theta * std::sin(phi), cos_theta };
+inline Vector3f spherical_dir(float sin_theta, float cos_theta, float phi)
+{
+    return Vector3f{sin_theta * std::cos(phi), sin_theta * std::sin(phi), cos_theta};
 }
-Vector3f reflect(const Vector3f& wi, const Vector3f& normal) {
+inline Vector3f reflect(const Vector3f &wi, const Vector3f &normal)
+{
     return 2 * dotProduct(normal, wi) * normal - wi;
 }
-std::optional<Vector3f> refract(const Vector3f& wi, const Vector3f& normal, float ior) {
+inline float fresnel(float cosi, float ior)
+{
+    float r0 = (1 - ior) / (1 + ior);
+    r0 = r0 * r0;
+    return r0 + (1 - r0) * std::pow((1.0f - cosi), 5);
+}
+std::optional<Vector3f> refract(const Vector3f &wi, const Vector3f &normal, float ior)
+{
     float cos_ni = dotProduct(normal, wi);
     Vector3f normal_real;
     float eta;
-    if (cos_ni > 0) {
+    if (cos_ni > 0)
+    {
         eta = 1.f / ior;
         normal_real = normal;
     }
-    else {
+    else
+    {
         cos_ni = -cos_ni;
         eta = ior;
         normal_real = -normal;
@@ -26,7 +38,8 @@ std::optional<Vector3f> refract(const Vector3f& wi, const Vector3f& normal, floa
 
     float sin_2_theta = (eta * eta * (1.0f - cos_ni * cos_ni));
     float cos_2_theta = 1.0f - sin_2_theta;
-    if (cos_2_theta >= 0) {
+    if (cos_2_theta >= 0)
+    {
         float cos_theta = std::sqrt(cos_2_theta);
         float nK = (eta * cos_ni) - cos_theta;
         Vector3f res = -wi * eta + normal_real * nK;
@@ -51,40 +64,48 @@ inline Vector3f sample_cos_hemisphere(const Vector2f &rnd_param)
         return Vector3f{ x, y,z };*/
 }
 
-struct luminance_op {
-    float operator()(const Lambertian& mats) const {
+struct luminance_op
+{
+    float operator()(const Lambertian &mats) const
+    {
         Vector3f diffuse = eval_texture(mats.reflectance, inter.uv, textures);
 
         if (mats.diffuse_map.has_value())
         {
             Vector3f diffuse1 = eval_texture(mats.diffuse_map.value(), inter.uv, textures);
-            if (diffuse.norm() > 0) diffuse = diffuse * diffuse1;
-            else diffuse = diffuse1;
+            if (diffuse.norm() > 0)
+                diffuse = diffuse * diffuse1;
+            else
+                diffuse = diffuse1;
         }
         return Luminance(diffuse);
     }
 
-    float operator()(const Specular& mats) const {
+    float operator()(const Specular &mats) const
+    {
         Vector3f specular = eval_texture(mats.specular, inter.uv, textures);
 
         return Luminance(specular);
     }
-    float operator()(const Transparent& mats)const {
+    float operator()(const Transparent &mats) const
+    {
         Vector3f tr = eval_texture(mats.transparent, inter.uv, textures);
         return Luminance(tr);
     }
-    float operator()(const Phong& mats) const {
-        return luminance_op::operator()(Lambertian{ mats.diffuse, mats.diffuse_map }) +
-            luminance_op::operator()(Specular{ mats.specular, mats.spec_exponent }) +
-            luminance_op::operator()(Transparent{ mats.transparent, mats.ior });
+    float operator()(const Phong &mats) const
+    {
+        return luminance_op::operator()(Lambertian{mats.diffuse, mats.diffuse_map}) +
+               luminance_op::operator()(Specular{mats.specular, mats.spec_exponent}) +
+               luminance_op::operator()(Transparent{mats.transparent, mats.ior});
     }
 
-    const Intersection& inter;
-    const std::vector<Image>& textures;
+    const Intersection &inter;
+    const std::vector<Image> &textures;
 };
 
-float luminance(const Material& material, const Intersection& inter, const std::vector<Image>& texture) {
-    return std::visit(luminance_op{ inter, texture }, material);
+float luminance(const Material &material, const Intersection &inter, const std::vector<Image> &texture)
+{
+    return std::visit(luminance_op{inter, texture}, material);
 }
 
 struct eval_op
@@ -101,26 +122,33 @@ struct eval_op
         if (mats.diffuse_map.has_value())
         {
             Vector3f diffuse1 = eval_texture(mats.diffuse_map.value(), vertex.uv, texture_pool);
-            if (diffuse.norm() > 0) diffuse = diffuse * diffuse1;
-            else diffuse = diffuse1;
+            if (diffuse.norm() > 0)
+                diffuse = diffuse * diffuse1;
+            else
+                diffuse = diffuse1;
         }
         return diffuse / M_PI * dotProduct(vertex.normal, dir_out);
     }
-    Vector3f operator()(const Specular& mats) const {
+    Vector3f operator()(const Specular &mats) const
+    {
         Vector3f r = reflect(dir_out, vertex.normal).normalized();
         float alpha = dotProduct(dir_in, r);
-        if (alpha > 0) {
+        if (alpha > 0)
+        {
             Vector3f ks = eval_texture(mats.specular, vertex.uv, texture_pool);
             Vector3f ns = eval_texture(mats.spec_exponent, vertex.uv, texture_pool);
-            Vector3f color = ks * (ns[0] + 2) * (0.5f / M_PI) * std::pow(alpha, ns[0]) * dotProduct(vertex.normal, dir_out);;
-            if (color[0] > 500 || color[1] > 500 || color[2] > 500) {
+            Vector3f color = ks * (ns[0] + 2) * (0.5f / M_PI) * std::pow(alpha, ns[0]) * dotProduct(vertex.normal, dir_out);
+            ;
+            if (color[0] > 500 || color[1] > 500 || color[2] > 500)
+            {
                 std::cout << ks << " " << ns << " " << std::endl;
             }
             return color;
         }
         return Vector3f(0.0);
     }
-    Vector3f operator()(const Transparent& mats) const {
+    Vector3f operator()(const Transparent &mats) const
+    {
         return Vector3f(0);
     }
     Vector3f operator()(const Phong &bsdf) const
@@ -133,15 +161,12 @@ struct eval_op
         // Vector3f ni = eval_texture(bsdf.ior, vertex.uv, texture_pool);
         // if (ni == 1)
 
-
-       
         Lambertian diffuse_comp = Lambertian{bsdf.diffuse, bsdf.diffuse_map};
         Specular spec_comp = Specular{bsdf.specular, bsdf.spec_exponent};
         Vector3f diffuse_color = eval(diffuse_comp, dir_in, dir_out, vertex, texture_pool);
         Vector3f spec_color = eval(spec_comp, dir_in, dir_out, vertex, texture_pool);
-        //std::cerr << "diffuse: " << diffuse_color << " spec: " << spec_color << std::endl;
-        return diffuse_color + spec_color;// *dotProduct(vertex.normal, dir_out);
-
+        // std::cerr << "diffuse: " << diffuse_color << " spec: " << spec_color << std::endl;
+        return diffuse_color + spec_color; // *dotProduct(vertex.normal, dir_out);
     }
     const Vector3f &dir_in;
     const Vector3f &dir_out;
@@ -160,59 +185,62 @@ eval(const Material &material,
 }
 struct sample_bsdf_op
 {
-    std::tuple<std::optional<Vector3f>, float>  operator()(const Lambertian &bsdf) const
+    std::tuple<std::optional<Vector3f>, float> operator()(const Lambertian &bsdf) const
     {
         if (dotProduct(vertex.normal, dir_in) < 0)
         {
-            return { std::nullopt, 0 };
-        }
-        Coordinate coord(vertex.normal);
-        Vector3f v = (to_world(coord, sample_cos_hemisphere(uv)).normalized());
-        return { v, dotProduct(v, vertex.normal) / M_PI };
-    }
-    std::tuple<std::optional<Vector3f>, float>  operator()(const Phong &bsdf) const
-    {
-        if (dotProduct(vertex.normal, dir_in) < 0)
-        {
-            return { std::nullopt, 0 };
+            return {std::nullopt, 0};
         }
         Coordinate coord(vertex.normal);
         Vector3f v = (to_world(coord, sample_cos_hemisphere(uv)).normalized());
         return {v, dotProduct(v, vertex.normal) / M_PI};
     }
-    std::tuple<std::optional<Vector3f>, float>  operator()(const Specular& bsdf) const
+    std::tuple<std::optional<Vector3f>, float> operator()(const Phong &bsdf) const
     {
-        //Sample a direction on the hemisphere for the half-vector
+        if (dotProduct(vertex.normal, dir_in) < 0)
+        {
+            return {std::nullopt, 0};
+        }
+        Coordinate coord(vertex.normal);
+        Vector3f v = (to_world(coord, sample_cos_hemisphere(uv)).normalized());
+        return {v, dotProduct(v, vertex.normal) / M_PI};
+    }
+    std::tuple<std::optional<Vector3f>, float> operator()(const Specular &bsdf) const
+    {
+        // Sample a direction on the hemisphere for the half-vector
         float exponent = eval_texture(bsdf.spec_exponent, vertex.uv, texture_pool)[0];
         float cos_theta = std::pow(uv.x, 1 / (exponent + 1));
         float sin_theta = std::sqrt(std::max(0.f, 1 - cos_theta * cos_theta));
         float phi = uv.y * 2 * M_PI;
         Vector3f half = spherical_dir(sin_theta, cos_theta, phi);
-        if (dir_in[2] *  half[2] < 0) {
+        if (dir_in[2] * half[2] < 0)
+        {
             half = -half;
         }
-        //The sampled incident direction is the outgoing direction reflected about the half-vector
+        // The sampled incident direction is the outgoing direction reflected about the half-vector
         Vector3f dir_out = reflect(dir_in, half);
         float pdf = ((exponent + 1) * std::pow(cos_theta, exponent)) / (2 * M_PI * 4 * dotProduct(dir_out, half));
-        if (dotProduct(dir_out, half) <= 0) {
+        if (dotProduct(dir_out, half) <= 0)
+        {
             pdf = 0;
         }
-        return { dir_out, pdf };
+        return {dir_out, pdf};
     }
-    std::tuple<std::optional<Vector3f>, float>  operator()(const Transparent& bsdf) const {
+    std::tuple<std::optional<Vector3f>, float> operator()(const Transparent &bsdf) const
+    {
         return {std::nullopt, 0};
     }
     const Vector3f &dir_in;
     const Intersection &vertex;
     const std::vector<Image> &texture_pool;
     const Vector2f &uv;
-    //const float &w;
+    // const float &w;
 };
-std::tuple<std::optional<Vector3f>, float>  sample_bsdf(const Material &material,
-                                    const Vector3f &dir_in,
-                                    const Intersection &inter,
-                                    const std::vector<Image> &texture,
-                                    const Vector2f &uv)
+std::tuple<std::optional<Vector3f>, float> sample_bsdf(const Material &material,
+                                                       const Vector3f &dir_in,
+                                                       const Intersection &inter,
+                                                       const std::vector<Image> &texture,
+                                                       const Vector2f &uv)
 {
     return std::visit(sample_bsdf_op{dir_in, inter, texture, uv}, material);
 }
@@ -239,7 +267,7 @@ struct pdf_sample_bsdf_op
         return fmax(dotProduct(vertex.normal.normalized(), dir_out.normalized()), 0.0f) / M_PI;
         return 0.5f / M_PI;
     }
-    float operator()(const Transparent& bsdf) const
+    float operator()(const Transparent &bsdf) const
     {
         if (dotProduct(vertex.normal, dir_in) < 0 ||
             dotProduct(vertex.normal, dir_out) < 0)
@@ -250,7 +278,7 @@ struct pdf_sample_bsdf_op
         return fmax(dotProduct(vertex.normal.normalized(), dir_out.normalized()), 0.0f) / M_PI;
         return 0.5f / M_PI;
     }
-    float operator()(const Specular& bsdf) const
+    float operator()(const Specular &bsdf) const
     {
         if (dotProduct(vertex.normal, dir_in) < 0 ||
             dotProduct(vertex.normal, dir_out) < 0)
